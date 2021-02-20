d07f736583b30b5593a85c0784105ac39ea4cf48
Sankichi Okute <higebobo@gmail.com>
2021-02-20 20:47:04 +0900

    modified

--- app/main.py
+++ app/main.py
@@ -1,33 +1,39 @@
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.middleware.cors import CORSMiddleware
 
 from app.service.database import database
 from app.users.endpoints import router as userrouter
 
 app = FastAPI()
 
-# 起動時にDatabaseに接続する。
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 
 @app.on_event("startup")
 async def startup():
+    """# 起動時にDatabaseに接続する。"""
     await database.connect()
-
-# 終了時にDatabaseを切断する。
 
 
 @app.on_event("shutdown")
 async def shutdown():
+    """# 終了時にDatabaseを切断する。"""
     await database.disconnect()
 
 # users routerを登録する。
 app.include_router(userrouter)
 
-# middleware state.connectionにdatabaseオブジェクトをセットする。
-
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
+    """# middleware state.connectionにdatabaseオブジェクトをセットする。"""
     request.state.connection = database
     response = await call_next(request)
     return response
